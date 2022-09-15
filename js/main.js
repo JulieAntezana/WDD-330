0894b3c71672ec954e010b66f31d3478a8a2c076
JulieAntezana <jantezana@byui.edu>
2022-09-15 14:43:14 -0500

    double click button

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ const links = [
   }
 ]
 
-function showCongratulations(button){
+function showCongratulations(theButtonClicked){
   document.getElementById('result_display').innerHTML =
-    "Congratulations, you did it!<p>You got the page to update!</p>"}
+    "Congratulations, you did it!<p>You got the page to update!</p>"
+  theButtonClicked.innerHTML = "Click Me Again!"  
+}
