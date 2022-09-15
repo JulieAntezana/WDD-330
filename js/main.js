dae03f9082b33e91113001d1a61eb220fdfbff98
JulieAntezana <jantezana@byui.edu>
2022-09-15 14:12:18 -0500

    fix first event

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 const links = [
   {
     label: "Week1 notes",
-    url: "week1/index.html"
+    url: "week1/index.html",
+    label: "First Event",
+    url: "first_event.html",
+    label: "Week2 notes",
+    url: "week2/index.html"
   }
 ]
+
+function showCongratulations(theButtonClicked){
+  document.getElementById('result_display').innerHTML =
+    "Congratulations, you did it!<p>You got the page to update!</p>"}
