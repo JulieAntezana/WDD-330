a4bf7e225d1f129362fb34a1b2d82afb8eb6f58e
JulieAntezana <jantezana@byui.edu>
2022-11-06 04:50:04 -0600

    Update Transforms

--- week8/transformations/main.js
+++ week8/transformations/main.js
@@ -1,9 +1,9 @@
 
-let canvas = document.getElementById("myCanvas")
-let context = canvas.getContext("2d")
+// let canvas = document.getElementById("myCanvas")
+// let context = canvas.getContext("2d")
 
-context.strokeStyle = "blue"
-context.fillStyle = "lightblue"
+// context.strokeStyle = "blue"
+// context.fillStyle = "lightblue"
 
-context.fillRect(10,10,100,100)
-context.strokeRect(10,10,100,100)
+// context.fillRect(10,10,100,100)
+// context.strokeRect(10,10,100,100)
